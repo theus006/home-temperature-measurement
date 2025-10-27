1995cd154046adacc65329c7f1dd660dc004dce8
theus006 <matheuscostadeo@gmail.com>
2025-10-27 19:39:59 -0300

    updating color, images and server.js

--- node/server.js
+++ node/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes/measureRoutes");
+require("dotenv").config();
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("./public"));
 app.use("/", routes);
 
-app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000");
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor rodando na porta ${process.env.PORT}`);
 });
